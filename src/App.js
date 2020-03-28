abd51ff24fd9f8da31420c5123a90a8053a23a86
Cezar Neaga <hi@cezarneaga.eu>
2020-03-28 16:29:15 +0100

    add some indicators

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import analytics from './analytics.json'
+import { Flex, Box, Text } from '@chakra-ui/core'
+// import useSWR from 'swr'
+// import fetcher from './utils/fetcher'
 import {
   LineChart,
   Line,
@@ -9,7 +12,14 @@ import {
   Tooltip,
   Legend,
 } from 'recharts'
+import Header from './Header'
+// const zone = process.env.REACT_APP_ZONE
+// const endpoint = `/client/v4/zones/${zone}/analytics/dashboard?since=2019-04-01T12:23:00Z&until=2020-03-27T12:23:00Z&continuous=false`
+
 function App() {
+  // const { data, error } = useSWR(endpoint, fetcher, {
+  //   shouldRetryOnError: false,
+  // })
   const dataJson = analytics.timeseries.map(day => {
     return {
       day: new Date(day.until).toLocaleString('en-US', {
@@ -18,17 +28,64 @@ function App() {
         month: 'short',
         day: 'numeric',
       }),
-      unique: day.uniques.all,
+      uniques: day.uniques.all,
       pageViews: day.pageviews.all,
     }
   })
   return (
     <>
+      <Header />
+      <Flex align="center" my="10">
+        <Box
+          size="33%"
+          p="10"
+          mx="10"
+          align="center"
+          justify="center"
+          borderWidth="1px"
+          rounded="lg"
+          overflow="hidden"
+          borderColor="gray.200">
+          <Text>Total Unique Visitors</Text>
+          <Text fontSize="4xl">{analytics.totals.uniques.all}</Text>
+        </Box>
+        <Box
+          size="33%"
+          p="10"
+          mx="10"
+          align="center"
+          justify="center"
+          borderWidth="1px"
+          rounded="lg"
+          overflow="hidden"
+          borderColor="gray.200">
+          <Text>Maximum Unique Visitors</Text>
+          <Text fontSize="4xl">
+            {Math.max(...dataJson.map(a => a.uniques))}
+          </Text>
+        </Box>
+        <Box
+          size="33%"
+          p="10"
+          mx="10"
+          align="center"
+          justify="center"
+          borderWidth="1px"
+          rounded="lg"
+          overflow="hidden"
+          borderColor="gray.200">
+          <Text>Minimum Unique Visitors</Text>
+          <Text fontSize="4xl">
+            {Math.min(...dataJson.map(a => a.uniques))}
+          </Text>
+        </Box>
+      </Flex>
+
       <LineChart
-        width={1400}
+        width={1280}
         height={500}
         data={dataJson}
-        margin={{ top: 30, right: 30, left: 20, bottom: 5 }}>
+        margin={{ top: 30, right: 0, left: 0, bottom: 5 }}>
         <XAxis dataKey="day" />
         <YAxis />
         <CartesianGrid strokeDasharray="3 3" />
@@ -36,13 +93,14 @@ function App() {
         <Legend />
         <Line
           type="monotone"
-          dataKey="unique"
+          dataKey="uniques"
           stroke="#8884d8"
           activeDot={{ r: 3 }}
         />
       </LineChart>
+
       <LineChart
-        width={1400}
+        width={1280}
         height={500}
         data={dataJson}
         margin={{ top: 30, right: 30, left: 20, bottom: 5 }}>
